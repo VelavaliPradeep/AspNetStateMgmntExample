e1cb59beaec465058618c908b93e42e02b0b1ce5
Your Name <you@example.com>
2019-07-22 10:18:26 +0530

    initialcommit

---
+++ StateMgmtExample/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateMgmtExample.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string userTxt, string userPass)
+        {
+            if (userTxt == "pradeep" && userPass == "12345")
+            {
+                //HttpCookie kt1 = new HttpCookie("username", userTxt);
+                //Response.Cookies.Add(kt1);
+
+                //Response.Cookies["password"].Value = userPass;
+
+                Session["username"] = userTxt;
+                Session["password"] = userPass;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
